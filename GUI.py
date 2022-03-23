d9116950e003245d908f877c8a9373548bca9d5d
Joseph Baker <94914495+JosephECSE1@users.noreply.github.com>
2022-03-23 12:15:15 +0000

    Add files via upload
    hi Mark this is your GUI

---
+++ GUI.py
@@ -0,0 +1,56 @@
+
+import sys
+import numpy   #unneeded rn
+import pygame
+from pygame.locals import *
+
+from Sprites import Obstacle, Bus
+import constants
+
+
+class GUI:
+    def __init__(self):
+        pygame.init()
+        self.fps = 24    
+        self.fpsClock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT), 0, 32)
+        pygame.display.set_caption("Kinchbus Kollision Avoidance System GUI")
+
+        self.screen.fill(constants.white)
+        self.all_sprites = pygame.sprite.Group()
+        self.bus = Bus()
+        self.O = Obstacle('Person', 90, 15)
+        self.all_sprites.add(self.bus, self.O)
+        self.all_sprites.draw(self.screen)
+
+    def draw_distance(self):
+        start = (constants.WIDTH/2,constants.HEIGHT/2)
+        end = (self.O.x, self.O.y)
+        pygame.draw.line(self.screen, constants.red, start, end, 3)
+        font = pygame.font.SysFont('Calibri', 20, True, False)
+        text = font.render(str(self.O.distance)+'cm', True, constants.white, constants.red)
+        self.screen.blit(text, ((start[0]+end[0])/2-20, (start[1]+end[1])/2-10))
+
+    def run(self):
+        while True:
+            
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            #update display
+            pygame.display.update()
+            self.all_sprites.update()
+
+            #display the display
+            self.screen.fill(constants.white)
+            self.draw_distance()
+            self.all_sprites.draw(self.screen)          
+
+            self.fpsClock.tick(self.fps)
+
+if __name__ == '__main__':
+    gui = GUI()
+    gui.run()
---
+++ Sprites.py
@@ -0,0 +1,41 @@
+import pygame
+import spritesheet
+import constants
+import math
+
+joes_art = pygame.image.load(r"Images\Spritesheet.png")#.convert_alpha()
+sprite_sheet = spritesheet.SpriteSheet(joes_art)
+
+class Bus(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = sprite_sheet.get_image(0, 0, 22, 26, 4, constants.white).convert_alpha()
+        self.x = constants.WIDTH/2
+        self.y = constants.HEIGHT/2
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type, angle, distance):
+        super().__init__()
+        self.type = type
+        self.angle = angle
+        self.distance = distance
+        self.distance_sf = 10 #10pixels == 1cm. So it's 24cm from middle of bus to top of screen.
+
+        if angle > 0:
+            self.x = (constants.WIDTH/2) + round(math.sin(abs(angle)))*distance*self.distance_sf
+            print(self.x)
+        else:
+            self.x = (constants.WIDTH/2) - round(math.sin(abs(angle)))*distance*self.distance_sf
+            print(self.x)
+
+        self.y = (constants.HEIGHT/2) - round(math.cos(abs(angle)))*distance*self.distance_sf
+        print(self.y)
+
+        if self.type == 'Person':
+            self.image = sprite_sheet.get_image(30, 0, 9,23, 4, constants.white).convert_alpha()
+        elif self.type == 'Car':
+            self.image = sprite_sheet.get_image(60, 0, 30, 30, 4, constants.white).convert_alpha()
+
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+
