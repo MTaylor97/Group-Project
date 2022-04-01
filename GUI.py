5b103b5faceff3d5a32f44f59c290017e4bc863a
JosephECSE1 <joseph.baker16@outlook.com>
2022-04-01 13:10:28 +0100

    made stopping system

--- GUI.py
+++ GUI.py
@@ -10,8 +10,8 @@ class GUI:
     def __init__(self):
         pygame.init()
         
-        self.fps = 60 
-        self.fpsClock = pygame.time.Clock()
+        self.fps = 60 # for the basic run mode 
+        self.fpsClock = pygame.time.Clock() # --//--
 
         self.screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT), 0, 32)
         pygame.display.set_caption("Kinchbus Kollision Avoidance System GUI")
@@ -30,19 +30,24 @@ class GUI:
         self.foreground_sprites.add(self.bus)
 
     def obstacle_found(self, type, angle, distance):
+        # allows obstacles where there is no spritesheet class
         self.obstacle_sprites.add(Obstacle(self.sprite_sheet, type, angle, distance))
-
+    
+    def quit(self):
+        # allows pygame to be closed from GUI_detectv2
+        pygame.quit()
+        
     def hud(self):
         font = pygame.font.SysFont('Calibri', 20, True, False)
         colour = constants.red
         for sprite in self.obstacle_sprites:
-            if sprite.front[1] <= 0: # off screen
+            if sprite.front[1] <= 0: # if off screen
                 pygame.draw.polygon(self.screen, colour, [[sprite.x-10, 20],[sprite.x+10, 20],[sprite.x, 0]])
                 text = font.render(str(sprite.type), True, constants.white, colour)
                 self.screen.blit(text, (sprite.x-10, 20))
                 text = font.render(str(round(sprite.distance))+'cm', True, constants.white, colour)
                 self.screen.blit(text, (sprite.x-10, 40))
-                continue
+                continue # if off screen skip the rest of this
 
             if sprite.distance <= 20: # if close
                 start = (constants.WIDTH/2,constants.HEIGHT/2)
@@ -89,14 +94,21 @@ class GUI:
             self.fpsClock.tick(self.fps)
 
     def run_1_loop(self):
-    
+        stopped = False
+
         self.background_sprites.draw(self.screen)
         self.obstacle_sprites.draw(self.screen)
-        #self.hud()
+        self.hud()
         self.foreground_sprites.draw(self.screen)
 
-        
-        self.background_sprites.update() # moving background
+        ###LOGIC###
+        for sprite in self.obstacle_sprites:
+            if sprite.distance <= 20:
+                stopped = True
+
+        if not stopped:
+            self.background_sprites.update() # moving background
+
         self.obstacle_sprites.empty()
         pygame.display.update()
 
